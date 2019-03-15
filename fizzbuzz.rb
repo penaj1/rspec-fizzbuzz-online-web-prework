9b9b2432d179cc82defccbb7f6516c84ea2d5f7e
Janice Pena <jipena94@gmail.com>
2019-03-15 02:53:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,10 @@
 def fizzbuzz(int)
 if int % 3 == 0 and int % 5 == 0
   puts "Fizzbuzz"
-  elsif int % 3 == 0
-  puts "Fizz"
   elsif int % 5 == 0
   puts "Buzz"
+  elsif int % 3 == 0
+  puts "Fizz"
 else
   puts "nil"
 end
